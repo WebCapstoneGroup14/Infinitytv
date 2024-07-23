d8f6f4ca87bde4d05f5b30d2c053c6cc6d6e7d5c
patrickoreye <117451160+patrickoreye@users.noreply.github.com>
2024-07-22 23:47:21 -0400

    removed
    Co-Authored-By: mkaushal8203 <163463225+mkaushal8203@users.noreply.github.com>
    Co-Authored-By: SmeetChavda1705 <170677360+SmeetChavda1705@users.noreply.github.com>

--- Server/middlewares/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-// This is the Error Handling Middleware
-export const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode); // Corrected line
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
-};
